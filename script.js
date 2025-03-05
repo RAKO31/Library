9c94c3066a94951d74aaa9f8b88d86354c437602
RAKO31 <raghavkohli668@gmail.com>
2025-03-05 18:22:25 +0530

    inital commit

--- script.js
+++ script.js
@@ -10,13 +10,52 @@ function addBookToLibrary(book){
     myLibrary.push(book);
 }
 
-Book.prototype.showBook=function (){
-    return (`${this.name} by ${this.author} contains ${this.pages}.`);
+
+const bookList = document.querySelector(".bookList");
+const open = document.querySelector("[data-open-modal]");
+const modal = document.querySelector('dialog');
+const submit = document.querySelector(".add");
+const close = document.querySelector("[data-close-modal]");
+
+open.addEventListener("click",(e)=>{
+    modal.showModal();
+});
+
+function createCard(book) {
+    const bookDisplay = document.createElement("div");
+    bookDisplay.classList.add("bookDisplay");
+
+    bookDisplay.innerHTML = `
+        <p>NAME: ${book.name}</p>
+        <p>AUTHOR: ${book.author}</p>
+        <p>PAGES: ${book.pages}</p>
+    `;
+
+    const removeCard = document.createElement("button");
+    removeCard.textContent = "Remove";
+    removeCard.classList.add("removeBtn");
+    bookDisplay.appendChild(removeCard);
+
+    removeCard.addEventListener("click", () => {
+        bookList.removeChild(bookDisplay);
+    });
+
+    bookList.appendChild(bookDisplay);
 }
 
-const btn = document.querySelector(".add");
-const modal = document.querySelector('[data-modal]')
-
-btn.addEventListener('click',(e)=>{
-    modal.show();
+submit.addEventListener('click',(e)=>{
+    e.preventDefault();
+    let name = document.getElementById("bookname").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    const newBook = new Book(name,author,pages);
+    addBookToLibrary(newBook);
+    createCard(newBook);
+    document.getElementById("bookname").value = '';
+    document.getElementById("author").value = '';
+    document.getElementById("pages").value = '';
 })
+
+close.addEventListener("click",(e)=>{
+    modal.close();
+});
